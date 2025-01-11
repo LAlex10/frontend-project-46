702d6bd4a1841a7bb954cfc13f78a8267d3a775a
lexey <likovalex@yandex.ru>
2025-01-11 23:34:27 +0300

    gendiffArguments

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -6,5 +6,6 @@ const program = new Command();
 program
   .description('Compares two configuration files and shows a difference.')
   .version('0.8.0')
+  .arguments('<filepath1> <filepath2>')
   .option('-f, --format <type>', 'output format', 'stylish')
   .parse() 
