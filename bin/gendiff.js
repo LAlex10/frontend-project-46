0fe7e324d92ef739b1bb82033b5f74386e86791e
lexey <likovalex@yandex.ru>
2025-01-11 23:28:32 +0300

    firststepGendiff

---
+++ bin/gendiff.js
@@ -0,0 +1,10 @@
+#!/usr/bin/env node
+
+import { Command } from 'commander/esm.mjs';
+
+const program = new Command();
+program
+  .description('Compares two configuration files and shows a difference.')
+  .version('0.8.0')
+  .option('-f, --format <type>', 'output format', 'stylish')
+  .parse() 
